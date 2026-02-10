178c237b865b168e5d0171687194431af118d619
fredchinonso555-ui <fredchinonso555@gmail.com>
2026-02-10 09:29:23 +0100

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "2347071035915,2349251919746";   // You can replace this number with yours //
+  gg = "2347071035915,2349151919746";   // You can replace this number with yours //
 }
 
 
